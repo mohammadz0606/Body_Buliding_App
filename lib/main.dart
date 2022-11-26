d9314c6157e504dea9a02715dfb6a177a77dfccd
User <basharqasrawi118@gmail.com>
2022-11-26 12:06:07 +0300

    Bashar qasrawibg1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:body_building/constant/constant_style.dart';
-//
-// //
-// //
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
