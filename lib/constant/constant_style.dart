ff41b8710b865ed68eaeb5b3a3cd39bb8e0c6d9d
User <basharqasrawi118@gmail.com>
2022-12-06 12:50:42 +0300

    calculator

--- lib/constant/constant_style.dart
+++ lib/constant/constant_style.dart
@@ -5,4 +5,6 @@ class MyColors {
   static const Color secondColor = Color(0xff232441);
   static const Color thridColor = Color(0xff252638);
   static const Color titleFieldsColor = Color(0xff707070);
+  static const Color primaryColor = Color.fromRGBO(12,18,52,1);
+  static const Color secondaryColor = Color.fromRGBO(39,42,78, 1.0);
 }
--- lib/screens/calculate_screen.dart
+++ lib/screens/calculate_screen.dart
@@ -1,13 +1,288 @@
 import 'package:flutter/material.dart';
 
-class CalculateScreen extends StatelessWidget {
+import '../constant/constant_style.dart';
+
+class CalculateScreen extends StatefulWidget {
   const CalculateScreen({Key? key}) : super(key: key);
 
   @override
+  State<CalculateScreen> createState() => _CalculateScreenState();
+}
+
+class _CalculateScreenState extends State<CalculateScreen> {
+  double sliderVal = 0;
+  int age = 0;
+  int weight = 150;
+
+  @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
-      body: Center(
-        child: Text("Calculate Screen"),
+      appBar: AppBar(
+        title: const Text('BMI CALCULATOR'),
+        backgroundColor: MyColors.primaryColor,
+        centerTitle: true,
+        elevation: 50,
+      ),
+      /*
+
+      */
+      body: Stack(
+        children: [
+          Container(
+              padding: const EdgeInsets.all(20),
+              color: MyColors.primaryColor,
+              alignment: Alignment.center,
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        genderMethode(size, 'MALE', Icons.male),
+                        genderMethode(size, 'FEMALE', Icons.female)
+                      ],
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      color: MyColors.secondaryColor,
+                      margin: EdgeInsets.all(size.width * 0.04),
+                      width: double.infinity,
+                      height: size.height * 0.25,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(
+                                0, size.height * 0.01, 0, size.height * 0.03),
+                            child: Text(
+                              'HEIGHT',
+                              style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: size.width * 0.05),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                sliderVal.round().toString(),
+                                style: TextStyle(
+                                    fontSize: size.width * 0.1,
+                                    color: Colors.white),
+                              ),
+                              Text(
+                                ' cm',
+                                style: TextStyle(
+                                    fontSize: size.width * 0.07,
+                                    color: Colors.grey),
+                              )
+                            ],
+                          ),
+                          Slider(
+                              thumbColor: Colors.red,
+                              inactiveColor: Colors.grey,
+                              activeColor: Colors.white,
+                              divisions: 300,
+                              max: 300,
+                              min: 0,
+                              value: sliderVal,
+                              onChanged: (val) {
+                                setState(() {
+                                  sliderVal = val;
+                                });
+                              }),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.all(size.width * 0.01),
+                          width: size.width * 0.4,
+                          height: size.height * 0.25,
+                          alignment: Alignment.center,
+                          color: MyColors.secondaryColor,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.fromLTRB(
+                                    0, 0, 0, size.height * 0.02),
+                                child: Text(
+                                  'WEIGHT',
+                                  style: TextStyle(
+                                      fontSize: size.width * 0.050,
+                                      color: Colors.grey),
+                                ),
+                              ),
+                              Text(
+                                weight.toString(),
+                                style: TextStyle(
+                                    fontSize: size.width * 0.13,
+                                    color: Colors.white),
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  FloatingActionButton(
+                                    backgroundColor: Color(0x353956FF),
+                                    child: Icon(
+                                      Icons.add,
+                                      color: Colors.grey,
+                                    ),
+                                    onPressed: () {
+                                      setState(() {
+                                        weight += 1;
+                                      });
+                                    },
+                                  ),
+                                  FloatingActionButton(
+                                    backgroundColor: Color(0x353956FF),
+                                    child: Text(
+                                      '-',
+                                      style: TextStyle(
+                                          color: Colors.grey,
+                                          fontSize: size.width * 0.1),
+                                    ),
+                                    onPressed: () {
+                                      setState(() {
+                                        weight -= 1;
+                                      });
+                                    },
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.all(size.width * 0.01),
+                          width: size.width * 0.4,
+                          height: size.height * 0.25,
+                          alignment: Alignment.center,
+                          color: MyColors.secondaryColor,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.fromLTRB(
+                                    0, 0, 0, size.height * 0.02),
+                                child: Text(
+                                  'AGE',
+                                  style: TextStyle(
+                                      fontSize: size.width * 0.050,
+                                      color: Colors.grey),
+                                ),
+                              ),
+                              Text(
+                                age.toString(),
+                                style: TextStyle(
+                                    fontSize: size.width * 0.13,
+                                    color: Colors.white),
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  FloatingActionButton(
+                                    backgroundColor: Color(0x353956FF),
+                                    child: Icon(
+                                      Icons.add,
+                                      color: Colors.grey,
+                                    ),
+                                    onPressed: () {
+                                      setState(() {
+                                        age += 1;
+                                      });
+                                    },
+                                  ),
+                                  FloatingActionButton(
+                                    backgroundColor: Color(0x353956FF),
+                                    child: Text(
+                                      '-',
+                                      style: TextStyle(
+                                          color: Colors.grey,
+                                          fontSize: size.width * 0.1),
+                                    ),
+                                    onPressed: () {
+                                      setState(() {
+                                        age -= 1;
+                                      });
+                                    },
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        genderMethode(size, 'drying of fat'.toUpperCase(),
+                            Icons.sports_gymnastics_sharp),
+                        genderMethode(size, 'muscular'.toUpperCase(),
+                            Icons.sports_gymnastics_sharp)
+                      ],
+                    ),SizedBox(height: 25,)
+                  ],
+                ),
+              )),
+          Positioned(bottom:0,left:size.width/4,child: ElevatedButton(style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.red),
+          ),
+            onPressed: () {},
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'CALCULATE',
+                style: TextStyle(fontSize: size.width * 0.07),
+              ),
+            ),
+          ),)
+        ],
+      ),
+    );
+  }
+
+  InkWell genderMethode(
+    Size size,
+    String title,
+    IconData icon,
+  ) {
+    return InkWell(
+      onTap: () {
+        print(title);
+      },
+      child: Container(
+        margin: EdgeInsets.all(size.width * 0.01),
+        width: size.width * 0.4,
+        height: size.height * 0.2,
+        alignment: Alignment.center,
+        color: MyColors.secondaryColor,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 0, 0, size.height * 0.02),
+              child: Icon(
+                icon,
+                size: size.width * 0.28,
+                color: Colors.grey,
+              ),
+            ),
+            Text(
+              title,
+              style: TextStyle(fontSize: size.width * 0.05, color: Colors.grey),
+            )
+          ],
+        ),
       ),
     );
   }
