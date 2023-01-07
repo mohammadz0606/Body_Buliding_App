0e0cb08ce884416941159e21d7990d4fcd2483e2
mohammadz0606 <mho.samer147@gmail.com>
2023-01-07 11:00:05 +0300

    bugs_done15

--- lib/screens/auth/signup_screen.dart
+++ lib/screens/auth/signup_screen.dart
@@ -1,7 +1,6 @@
 import 'package:body_building/services/provider/app_provider.dart';
 import 'package:body_building/widgets/background_image_widget.dart';
 import 'package:body_building/widgets/title_fields.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
